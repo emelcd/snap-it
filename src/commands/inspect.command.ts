88575c5b744942390a08543a5aa3fdc68c1371c7
emelcd <mick.altura@gmail.com>
2023-02-27 01:37:31 +0100

    🎨 Cleaned info

--- src/commands/inspect.command.ts
+++ src/commands/inspect.command.ts
@@ -3,6 +3,7 @@ import { createConnection } from '../utils/db.utils'
 export async function inspectCommand (tag: string): Promise<void> {
   const { snaps } = await createConnection()
   const snapData = await snaps.findOne({ tag })
+
   if (snapData == null) {
     console.log('Snap not found')
     process.exit(0)
--- src/commands/preview.command.ts
+++ src/commands/preview.command.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { createConnection } from '../utils/db.utils'
-// import { createServer } from 'http'
+import http from 'http'
+import open from 'open'
 
 export async function previewCommand (tag: string): Promise<void> {
   try {
@@ -10,14 +11,24 @@ export async function previewCommand (tag: string): Promise<void> {
       console.log('Snap not found')
       process.exit(0)
     }
-    console.log(`FILE NAME: ${query.fileName}`)
-    console.log(`FILE EXTENSION: ${query.fileExtension}`)
-    console.log(`TAG: ${query.tag}`)
-    query.description && console.log(`DESCRIPTION: ${query.description}`)
-    // console.log(`DESCRIPTION: ${query.description}`)
-    console.log('')
-    console.log(query.fileContent.split('\n').slice(0, 10).join('\n'))
-    process.exit(0)
+    const server = http.createServer((req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end()
+    })
+    server.listen(3000, () => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      open('http://localhost:3000')
+      setTimeout(() => {
+        process.exit(0)
+      }, 2e3)
+    })
+    // console.log(`FILE NAME: ${query.fileName}`)
+    // console.log(`FILE EXTENSION: ${query.fileExtension}`)
+    // console.log(`TAG: ${query.tag}`)
+    // query.description && console.log(`DESCRIPTION: ${query.description}`)
+    // // console.log(`DESCRIPTION: ${query.description}`)
+    // console.log('')
+    // console.log(query.fileContent.split('\n').slice(0, 10).join('\n'))
   } catch (error) {
     console.log(error)
     process.exit(0)
