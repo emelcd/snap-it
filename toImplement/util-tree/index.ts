eaa6b5c3b85ccf647ecbca0ae315734d7eb06387
emelcd <mick.altura@gmail.com>
2023-02-27 04:29:24 +0100

    🗑️ Cleaned

--- toImplement/util-tree/index.ts
+++ toImplement/util-tree/index.ts
@@ -1,68 +1,67 @@
-import fs from 'fs';
-import { isText } from 'istextorbinary';
+import fs from 'fs'
+import { isText } from 'istextorbinary'
 
 interface FolderContents {
-    [key: string]: FolderContents | boolean | Object;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  [key: string]: FolderContents | boolean | Object
 }
 
-function readFolderRecursive(folderPath: string): FolderContents {
-    const folderContents: FolderContents = {};
+function readFolderRecursive (folderPath: string): FolderContents {
+  const folderContents: FolderContents = {}
 
-    const readFolder = (path: string, parentKey?: string) => {
-        const folderFiles = fs.readdirSync(path, { withFileTypes: true });
-        folderFiles.forEach(file => {
-            const fileName = file.name;
-            if (fileName.startsWith('.')) {
-                return;
-            }
-            const fileKey = parentKey ? `${parentKey}/${fileName}` : fileName;
-            if (file.isDirectory()) {
-                folderContents[fileKey] = {};
-                readFolder(`${path}/${fileName}`, fileKey);
-            } else {
-                const isFileText = isText(`${path}/${fileName}`);
-                if (isFileText) {
-                    folderContents[fileKey] = isFileText;
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const readFolder = (path: string, parentKey?: string) => {
+    const folderFiles = fs.readdirSync(path, { withFileTypes: true })
+    folderFiles.forEach(file => {
+      const fileName = file.name
+      if (fileName.startsWith('.')) {
+        return
+      }
+      const fileKey = parentKey ? `${parentKey}/${fileName}` : fileName
+      if (file.isDirectory()) {
+        folderContents[fileKey] = {}
+        readFolder(`${path}/${fileName}`, fileKey)
+      } else {
+        const isFileText = isText(`${path}/${fileName}`)
+        if (isFileText) {
+          folderContents[fileKey] = isFileText
+        }
+      }
+    })
+  }
 
-                }
-            }
-        });
-    }
+  readFolder(folderPath)
 
-    readFolder(folderPath);
-
-    return folderContents;
+  return folderContents
 }
-const folderPath = './node-express-realworld-example-app';
+const folderPath = './node-express-realworld-example-app'
 // const isText = (path: string) => {
 //   const textExtensions = ['.txt', '.md', '.js', '.ts'];
 //   const extension = path.slice(path.lastIndexOf('.'));
 //   return textExtensions.includes(extension);
 // };
-const folderContents = readFolderRecursive(folderPath);
-
-const objectData = {}
+const folderContents = readFolderRecursive(folderPath)
 
 // folder Contents is an object
 const entriesFolder = Object.entries(folderContents)
 // get the values that are equal to an empty object
 const emptyObjects = entriesFolder.filter(([key, value]) => {
-    return typeof value === 'object' && Object.keys(value).length === 0
+  return typeof value === 'object' && Object.keys(value).length === 0
 })
 
 // create a directory for each empty object in test_folder
 emptyObjects.forEach(([key, value]) => {
-    console.log(key)
-    if (fs.existsSync(`test_folder/${key}`)) return
-    fs.mkdirSync(`test_folder/${key}`, { recursive: true })
+  console.log(key)
+  if (fs.existsSync(`test_folder/${key}`)) return
+  fs.mkdirSync(`test_folder/${key}`, { recursive: true })
 })
 
 const entriesFiles = entriesFolder.filter(([key, value]) => {
-    return typeof value === 'boolean'
+  return typeof value === 'boolean'
 })
 
 // read the content of the files
 entriesFiles.forEach(([key, value]) => {
-    const data = fs.readFileSync(folderPath + '/' + key, 'utf8')
-    fs.writeFileSync(`test_folder/${key}`, data)
+  const data = fs.readFileSync(folderPath + '/' + key, 'utf8')
+  fs.writeFileSync(`test_folder/${key}`, data)
 })
