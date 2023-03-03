250f016cbd84ca1a76cb75f893bc73f7af22460d
emelcd <mick.altura@gmail.com>
2023-03-03 22:52:39 +0100

    ✅ Updated recursive load command

--- src/commands/load.command/recursiveLoad.ts
+++ src/commands/load.command/recursiveLoad.ts
@@ -1,8 +1,21 @@
+import { mkdirSync, writeFileSync } from 'fs'
 import { type ISnapRecursive } from '../../interfaces/snap.interface'
 
 export const recursiveLoad = async (data: any): Promise<void> => {
-  const d = data as ISnapRecursive
-  const { files } = d
-  console.log(files.map(n => n.filePath))
-  process.exit(0)
+  try {
+    const d = data as ISnapRecursive
+    const parentName = 'src'
+    const { files, folders } = d
+    folders.forEach((folderName) => {
+      mkdirSync(parentName + '/' + folderName, { recursive: true })
+    })
+    files.forEach((file) => {
+      writeFileSync(parentName + '/' + file.filePath, file.fileContent)
+    })
+    console.log(parentName + ' has been loaded')
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
 }
